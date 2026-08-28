d1c28bee2cdea16e1bad3da28ea0b29a47a01c69
yeimyrojas24401-cyber <yeimy.rojas.24401@gmail.com>
2026-08-28 02:34:03 -0600

    SerializeButtons

--- Assets/Clases/Clase02_Canvas/Scripts/UIManager.cs
+++ Assets/Clases/Clase02_Canvas/Scripts/UIManager.cs
@@ -1,12 +1,22 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    [SerializeField] private TMP_Text profileName;
+    [SerializeField] private Button playBut;
+    [SerializeField] private Button pauseBut;
+    [SerializeField] private Button settingsBut;
+    [SerializeField] private Button creditsBut;
+    [SerializeField] private Button exitBut;
+    [SerializeField] private Button backBut;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        profileName.text = "Player_01";
     }
 
     // Update is called once per frame
