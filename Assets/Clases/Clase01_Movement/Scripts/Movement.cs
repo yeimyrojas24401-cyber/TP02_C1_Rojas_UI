eaac48b9c7a226d9bd21e49db47ddd0b92bed6fa
yeimyrojas24401-cyber <yeimy.rojas.24401@gmail.com>
2026-08-27 19:31:50 -0600

    RotationSettings

--- Assets/Clases/Clase01_Movement/Scripts/Movement.cs
+++ Assets/Clases/Clase01_Movement/Scripts/Movement.cs
@@ -8,7 +8,15 @@ public class Movement : MonoBehaviour
     [SerializeField] private KeyCode moveRight = KeyCode.D; // The key to move the player right
     [SerializeField] private KeyCode moveDown = KeyCode.S; // The key to move the player down
     [SerializeField] private KeyCode moveLeft = KeyCode.A; // The key to move the player left
+    [SerializeField] private KeyCode rotationRight = KeyCode.E; // The key to rotate the player right
+    [SerializeField] private KeyCode rotationLeft = KeyCode.Q; // The key to rotate the player left
+    [SerializeField] private float rotationAngle = 10; // here we define a variable for the speed of the rotation of the player so that we can change it in the inspector of the unity editor
 
+    private void Awake()
+    {
+       
+    }
+    
     private void Update()
     {
         if (Input.GetKey(moveUp))
@@ -27,6 +35,16 @@ public class Movement : MonoBehaviour
         if (Input.GetKey(moveLeft))
         {
             transform.position += new Vector3(-moveSpeed * Time.deltaTime,0);
+        }
+        if (Input.GetKeyDown(rotationLeft))
+        {
+            // Rotate the player to the left
+            transform.Rotate(0, 0, rotationAngle);
+        }
+        if (Input.GetKeyDown(rotationRight))
+        {
+            // Rotate the player to the right
+            transform.Rotate(0, 0, -rotationAngle);
         }
     }
     
