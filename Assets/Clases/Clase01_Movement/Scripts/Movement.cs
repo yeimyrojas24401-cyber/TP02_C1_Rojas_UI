2eab4d99b08918496b505af422913fe1b7002030
yeimyrojas24401-cyber <yeimy.rojas.24401@gmail.com>
2026-08-27 19:01:41 -0600

    Update Movement.cs

--- Assets/Clases/Clase01_Movement/Scripts/Movement.cs
+++ Assets/Clases/Clase01_Movement/Scripts/Movement.cs
@@ -8,15 +8,19 @@ public class Movement : MonoBehaviour
         if (Input.GetKey(KeyCode.W))
         {
             // Move the player forward
-            transform.position = new Vector3(0, 1);
+            transform.position += new Vector3(0, 1);
         }
         if (Input.GetKey(KeyCode.D))
         {             // Move the player right
-            transform.position = new Vector3(1, 0);
+            transform.position += new Vector3(1, 0);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            transform.position = new Vector3(0, -1);
+            transform.position += new Vector3(0, -1);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(-1,0);
         }
     }
     
