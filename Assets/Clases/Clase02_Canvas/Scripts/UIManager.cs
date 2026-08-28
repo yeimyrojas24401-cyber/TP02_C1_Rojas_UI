5eb918724a83b7407946aeb77bac1d25b2b2f221
yeimyrojas24401-cyber <yeimy.rojas.24401@gmail.com>
2026-08-28 02:49:52 -0600

    renameSprites

--- Assets/Clases/Clase02_Canvas/Scripts/UIManager.cs
+++ Assets/Clases/Clase02_Canvas/Scripts/UIManager.cs
@@ -5,23 +5,23 @@ using UnityEngine.UI;
 public class UIManager : MonoBehaviour
 {
     [SerializeField] private TMP_Text profileName;
-    [SerializeField] private Button playBut;
-    [SerializeField] private Button pauseBut;
-    [SerializeField] private Button settingsBut;
-    [SerializeField] private Button creditsBut;
-    [SerializeField] private Button exitBut;
-    [SerializeField] private Button backBut;
+    [SerializeField] private Button btnPlay;
+    [SerializeField] private Button btnPause;
+    [SerializeField] private Button btnSettings;
+    [SerializeField] private Button btnCredits;
+    [SerializeField] private Button btnExit;
+    [SerializeField] private Button btnBack;
 
-
+    private void Awake()
+    {
+        btnPlay.onClick.AddListener();
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         profileName.text = "Player_01";
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    void OnPlayClicked(); // Create a function
+
 }
