071e18d3f9af3018c37f88e6841abd47fc264e09
yeimyrojas24401-cyber <yeimy.rojas.24401@gmail.com>
2026-08-28 03:51:45 -0600

    settingsCanvas

--- Assets/Clases/Clase02_Canvas/Scripts/mainMenu.cs
+++ Assets/Clases/Clase02_Canvas/Scripts/mainMenu.cs
@@ -2,29 +2,41 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UIManager : MonoBehaviour
+public class mainMenu : MonoBehaviour
 {
     [Header("Buttons")]
     [SerializeField] private Button btnPlay;
-    [SerializeField] private Button btnPause;
     [SerializeField] private Button btnSettings;
     [SerializeField] private Button btnCredits;
     [SerializeField] private Button btnExit;
-    [SerializeField] private Button btnBack;
+
+    [SerializeField] private GameObject mainMenuCanvas;
+    [SerializeField] private GameObject settingsCanvas;
 
     private void Awake()
     {
         btnPlay.onClick.AddListener(OnPlayClicked); // "When click in btnPlay ejecuta OnPlayClicked
+        btnSettings.onClick.AddListener(OnSettingsClicked);
+        settingsCanvas.SetActive(false);
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
-
+    private void OnDestroy()
+    {
+        btnPlay.onClick.RemoveAllListeners();
+    }
     private void OnPlayClicked () // this is the function called OnPlayClicked which is going to run when the btnPlay click
     {
-        Debug.Log("play has been clicked");
+        mainMenuCanvas.SetActive(false); //  When Play is clicked, this GameObject becomes inactive.
+    }
+    private void OnSettingsClicked ()
+    {
+        mainMenuCanvas.SetActive(false);
+        settingsCanvas.SetActive(true);
+
     }
 
 }
