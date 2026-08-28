85ea8fee2cd6399ba0240795f7c806843ec56d8b
yeimyrojas24401-cyber <yeimy.rojas.24401@gmail.com>
2026-08-28 04:50:30 -0600

    CreditsPanel

--- Assets/Clases/Clase02_Canvas/Scripts/mainMenu.cs
+++ Assets/Clases/Clase02_Canvas/Scripts/mainMenu.cs
@@ -9,15 +9,20 @@ public class mainMenu : MonoBehaviour
     [SerializeField] private Button btnSettings;
     [SerializeField] private Button btnCredits;
     [SerializeField] private Button btnExit;
+    [SerializeField] private Button btnBack;
 
     [SerializeField] private GameObject mainMenuCanvas;
-    [SerializeField] private GameObject settingsCanvas;
+    [SerializeField] private GameObject settingsPanel;
+    [SerializeField] private GameObject creditsPanel;
 
     private void Awake()
     {
         btnPlay.onClick.AddListener(OnPlayClicked); // "When click in btnPlay ejecuta OnPlayClicked
         btnSettings.onClick.AddListener(OnSettingsClicked);
-        settingsCanvas.SetActive(false);
+        btnBack.onClick.AddListener(OnBackClicked);
+        btnCredits.onClick.AddListener(OnCreditsClicked);
+        settingsPanel.SetActive(false);
+        creditsPanel.SetActive(false);
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -27,6 +32,9 @@ public class mainMenu : MonoBehaviour
     private void OnDestroy()
     {
         btnPlay.onClick.RemoveAllListeners();
+        btnSettings.onClick.RemoveAllListeners();
+        btnCredits.onClick.RemoveAllListeners();
+        btnBack.onClick.RemoveAllListeners();
     }
     private void OnPlayClicked () // this is the function called OnPlayClicked which is going to run when the btnPlay click
     {
@@ -35,7 +43,21 @@ public class mainMenu : MonoBehaviour
     private void OnSettingsClicked ()
     {
         mainMenuCanvas.SetActive(false);
-        settingsCanvas.SetActive(true);
+        settingsPanel.SetActive(true);
+        creditsPanel.SetActive(false);
+
+    }
+    private void OnCreditsClicked ()
+    {
+        creditsPanel.SetActive(true);
+        mainMenuCanvas.SetActive(false);
+        settingsPanel.SetActive(false);
+    }
+    private void OnBackClicked()
+    {
+        mainMenuCanvas.SetActive(true);
+        settingsPanel.SetActive(false);
+        creditsPanel.SetActive(false);
 
     }
 
