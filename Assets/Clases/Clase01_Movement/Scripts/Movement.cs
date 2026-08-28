45490bdb4eb83b6ee1d4a6ae63222f730b413de7
yeimyrojas24401-cyber <yeimy.rojas.24401@gmail.com>
2026-08-27 19:03:35 -0600

    moveSpeed_variable

--- Assets/Clases/Clase01_Movement/Scripts/Movement.cs
+++ Assets/Clases/Clase01_Movement/Scripts/Movement.cs
@@ -3,24 +3,25 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 // the purpose of this class is to handle the movement of the player character through the keyboard input
 {
+    [SerializeField] private float moveSpeed = 1f; // The speed at which the player moves
     private void Update()
     {
         if (Input.GetKey(KeyCode.W))
         {
             // Move the player forward
-            transform.position += new Vector3(0, 1);
+            transform.position += new Vector3(0, moveSpeed * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.D))
         {             // Move the player right
-            transform.position += new Vector3(1, 0);
+            transform.position += new Vector3(moveSpeed * Time.deltaTime, 0);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            transform.position += new Vector3(0, -1);
+            transform.position += new Vector3(0, -moveSpeed * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            transform.position += new Vector3(-1,0);
+            transform.position += new Vector3(-moveSpeed * Time.deltaTime,0);
         }
     }
     
